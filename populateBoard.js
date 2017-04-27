8506565689e5f65b0f290cf0523be128eef6ce60
jon-atto-bennett <jon-atto-bennett>
2017-04-28 11:41:04 +1200

    createBoard done working on populateBoard

---
+++ populateBoard.js
@@ -0,0 +1,7 @@
+function populateBoard(board) {
+  var boardCopy = [...board]
+  //fill board cells with trues or falses
+  return boardCopy
+}
+
+module.exports = populateBoard
---
+++ tests/populateBoard.js
@@ -0,0 +1,6 @@
+var test = require('tape');
+var populateBoard = require('../populateBoard');
+
+test('populateBoard with the correct number of true and false' , function(t){
+  var expected = []
+})
