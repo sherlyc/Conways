33ac914c24494fb205946cce4e484a6df9e908db
jon-atto-bennett <jon-atto-bennett>
2017-04-28 16:47:37 +1200

    countAliveNeighbours working

--- countAliveNeighbours.js
+++ countAliveNeighbours.js
@@ -1,6 +1,18 @@
-function countAliveNeighbours {
+var outOfBounds = require('./checkOutOfBounds')
 
+function countAliveNeighbours(cell, board) {
+var x = cell[0];
+var y = cell[1];
+var countAlive = 0;
 
+  for (var i = -1; i <= 1; i++ ){
+    for (var j = -1; j <= 1; j++ ){
+        if(!outOfBounds(board[ x + i ][ y + j ]) && board [ x + i ][ y + j ] == true ){
+          countAlive++
+        };
+    }
+  }
+  return countAlive;
 }
 
 module.exports = countAliveNeighbours
--- tests/checkOutOfBounds.js
+++ tests/checkOutOfBounds.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var createBoard = require('../createBoard')
 var populateBoard = require('../populateBoard');
 var checkBound = require('../checkOutOfBounds');
+var countAlive = require('../countAliveNeighbours')
 
 test('is checkOutOfBound working' , function(t){
   var size = 4;
@@ -14,6 +15,37 @@ test('is checkOutOfBound working' , function(t){
 
   t.equal(checkOutOfBound, true, "this cell is out of bound");
   t.equal(checkOutOfBound2, false, "this cell is not out of bound");
+  t.end();
+
+})
+
+
+test('is countAliveNeighbours working - all of them dead' , function(t){
+  var size = 3;
+  var board = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ]
+  var board2 = [
+    [false, true, false],
+    [false, false, false],
+    [false, false, false],
+  ]
+  var board3 = [
+    [false, true, false],
+    [false, false, false],
+    [false, false, true],
+  ]
+  var cell = [1,1];
+  var countAliveNeighbours = countAlive(cell, board);
+  var countAliveNeighbours2 = countAlive(cell, board2);
+  var countAliveNeighbours3 = countAlive(cell, board3);
+
+
+  t.equal(countAliveNeighbours, 0, "no alive neighbours");
+  t.equal(countAliveNeighbours2, 1, "one alive neighbour");
+  t.equal(countAliveNeighbours3, 2, "two alive neighbours");
   t.end();
 
 })
